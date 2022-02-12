fc0e84924b206716d05214b8fcf601b1f859c165
noreikam <noreikam1@gmail.com>
2022-02-12 02:28:11 -0500

    password criteria validated

--- script.js
+++ script.js
@@ -13,18 +13,28 @@ var generateBtn = document.querySelector("#generate");
 
 // }
 var writePassword = function() {
-  // elicit password criteria and store in password object
-  var password = {
-    length: window.prompt("Input paassword length."),
-    lowerCase: window.confirm("Include lowercase letters?  OK for yes or cancel for no"),
-    upperCase: window.confirm("Include uppercase letters?  OK for yes or cancel for no"),
-    specialChar: window.confirm("Include special characters?  OK for yes or cancel for no"),
-    text: []  
+  // elicit and validate password length 
+  var length = parseInt(window.prompt("Input password length."));
+  while (length < 8 || length > 128 || isNaN(length)) {
+    length = parseInt(window.prompt("Length must be a number between 8 and 128.  Please select a valid length."));
   }
-  console.log(JSON.stringify(password));
+  
+  // elicit and validate character set
+  var lowerCase = window.confirm("Include lowercase letters?  OK for yes or cancel for no");
+  var upperCase = window.confirm("Include uppercase letters?  OK for yes or cancel for no");
+  var numeric = window.confirm("Include numbers?  OK for yes or cancel for no");
+  var specialChar = window.confirm("Include special characters?  OK for yes or cancel for no");
+  while (!lowerCase && !upperCase && !numeric && !specialChar) {
+    window.alert("At least one character type must be selected.  Please select at least one of lowercase letters, uppercase letters, or special characters.");
+    lowerCase = window.confirm("Include lowercase letters?  OK for yes or cancel for no");
+    upperCase = window.confirm("Include uppercase letters?  OK for yes or cancel for no");
+    var numeric = window.confirm("Include numbers?  OK for yes or cancel for no");
+    specialChar = window.confirm("Include special characters?  OK for yes or cancel for no");
+  }
 
-
+  console.log(numeric);
 }
-
+  
+  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
